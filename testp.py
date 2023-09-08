3b6c6f6184dda3db7836c2bf4be0a32d49eff3e1
YeeJieYao <1221402113@student.mmu.edu.my>
2023-09-08 23:51:33 +0800

    Update testp.py

--- testp.py
+++ testp.py
@@ -29,41 +29,41 @@ app.layout = html.Div(
 
 
 # Single Input, single Output, State, prevent initial trigger of callback, PreventUpdate
-@app.callback(
-    Output(component_id='graph-output', component_property='figure'),
-    [Input(component_id='my-dropdown', component_property='value')],
+#@app.callback(
+   # Output(component_id='graph-output', component_property='figure'),
+    #[Input(component_id='my-dropdown', component_property='value')],
     #[Input(component_id='my-button', component_property='n_clicks')],
     # [State(component_id='my-dropdown', component_property='value')],
-    prevent_initial_call=False
-)
-def update_my_graph(val_chosen):
-    if len(val_chosen) > 0:
+    #prevent_initial_call=False
+#)
+#def update_my_graph(val_chosen):
+   # if len(val_chosen) > 0:
         # print(n)
-        print(f"value user chose: {val_chosen}")
-        print(type(val_chosen))
-        dff = df[df["state"].isin(val_chosen)]
-        fig = px.pie(dff, values="pop", names="state", title="Malaysia Population")
-        fig.update_traces(textinfo="value+percent").update_layout(title_x=0.5)
-        return fig
-    elif len(val_chosen) == 0:
-        raise dash.exceptions.PreventUpdate
+     #   print(f"value user chose: {val_chosen}")
+    #    print(type(val_chosen))
+    #    dff = df[df["state"].isin(val_chosen)]
+     #   fig = px.pie(dff, values="pop", names="state", title="Malaysia Population")
+     #   fig.update_traces(textinfo="value+percent").update_layout(title_x=0.5)
+     #   return fig
+  #  elif len(val_chosen) == 0:
+   #     raise dash.exceptions.PreventUpdate
 
 
 # Multiple Input, multiple Output, dash.no_update
-# @app.callback(
-#     [Output('graph-output', 'figure'), Output('sentence-output', 'style')],
-#     [Input(component_id='my-radioitem', component_property='value'),
-#      Input(component_id='my-dropdown', component_property='value')],
-#     prevent_initial_call=False
-# )
-# def update_graph(color_chosen, val_chosen):
-#     if len(val_chosen) == 0:
-#         return dash.no_update, {"color": color_chosen}
-#     else:
-#         dff = df[df["fund_extended_name"].isin(val_chosen)]
-#         fig = px.pie(dff, values="ytd_return", names="fund_extended_name", title="Year-to-Date Returns")
-#         fig.update_traces(textinfo="value+percent").update_layout(title_x=0.5)
-#         return fig, {"color": color_chosen}
+@app.callback(
+     [Output('graph-output', 'figure'), Output('sentence-output', 'style')],
+     [Input(component_id='my-radioitem', component_property='value'),
+      Input(component_id='my-dropdown', component_property='value')],
+     prevent_initial_call=False
+ )
+def update_graph(color_chosen, val_chosen):
+    if len(val_chosen) == 0:
+        return dash.no_update, {"color": color_chosen}
+    else:
+        dff = df[df["fund_extended_name"].isin(val_chosen)]
+        fig = px.pie(dff, values="ytd_return", names="fund_extended_name", title="Year-to-Date Returns")
+        fig.update_traces(textinfo="value+percent").update_layout(title_x=0.5)
+        return fig, {"color": color_chosen}
 
 
 if __name__ == '__main__':
