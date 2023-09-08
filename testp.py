b5523e2fea72f75e18995420effd5fc6d79fb302
YeeJieYao <1221402113@student.mmu.edu.my>
2023-09-08 21:37:14 +0800

    Update testp.py

--- testp.py
+++ testp.py
@@ -1,6 +1,7 @@
 from dash import Dash, html, dcc, Output, Input
 import plotly.express as px
 import pandas as pd
+import dash
 
 app = Dash(__name__)
 server = app.server
@@ -24,7 +25,24 @@ app.layout = html.Div(
     ]
 )
 
-
+@app.callback(
+    Output(component_id='graph-output', component_property='figure'),
+    [Input(component_id='my-dropdown', component_property='value')],
+    # [Input(component_id='my-button', component_property='n_clicks')],
+    # [State(component_id='my-dropdown', component_property='value')],
+    prevent_initial_call=False
+)
+def update_my_graph(val_chosen):
+    if len(val_chosen) > 0:
+        # print(n)
+        print(f"value user chose: {val_chosen}")
+        print(type(val_chosen))
+        dff = df[df["fund_extended_name"].isin(val_chosen)]
+        fig = px.pie(dff, values="ytd_return", names="fund_extended_name", title="Year-to-Date Returns")
+        fig.update_traces(textinfo="value+percent").update_layout(title_x=0.5)
+        return fig
+    elif len(val_chosen) == 0:
+        raise dash.exceptions.PreventUpdate
 
 
 
