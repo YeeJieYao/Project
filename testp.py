ecedd6e14b877cae073084175b7ff13481656bd8
YeeJieYao <1221402113@student.mmu.edu.my>
2023-09-08 22:14:58 +0800

    Update testp.py

--- testp.py
+++ testp.py
@@ -10,37 +10,21 @@ image_path = 'https://www.mmu.edu.my/wp-content/themes/mmu2018/assets/images/log
 data_path = 'https://raw.githubusercontent.com/YeeJieYao/Project/main/population.csv'
 
 df = pd.read_csv(data_path)
-fig = px.pie(df,values='pop_60', names='state',title='Malaysia Population') .update_layout(xaxis_title="State", yaxis_title="Index")
+fig = px.pie(df,values='pop', names='state',title='Malaysia Population') .update_layout(xaxis_title="State", yaxis_title="Index")
 
-colors = ["black", "blue", "red", "yellow", "pink", "orange"]
+app.layout = html.Div([
+        html.Div([
+            html.Pre(children= "Population",
+            style={"text-align": "center", "font-size":"100%", "color":"black"})
+        ]),
 
-app.layout = html.Div(
-    children=[html.H1("Data Visualization"),
-    dcc.Dropdown(['Johor', 'Kedah', 'Kelantan','Melaka', 'Negeri Sembilan', 'Pahang', 'Pulau Pinang', 'Perak', 'Perlis', 'Selangor', 'Terengganu', 'Sabah', 'Sarawak', 'W.P. Kuala Lumpur', 'W.P. Labuan', 'W.P. Putrajaya'],
-                 'Johor', id='my-dropdown'),
-    html.Button(id='my-button', n_clicks=0, children="Show breakdown"),
-    dcc.Graph(id='graph-output', figure ={})
-    ]
-)
+        html.Div([
+            dcc.Graph(id='the_graph')
+        ])
 
-@app.callback(
-    Output(component_id='graph-output', component_property='figure'),
-    [Input(component_id='my-dropdown', component_property='value')],
-    # [Input(component_id='my-button', component_property='n_clicks')],
-    # [State(component_id='my-dropdown', component_property='value')],
-    prevent_initial_call=False
-)
-def update_my_graph(val_chosen):
-    if len(val_chosen) > 0:
-        # print(n)
-        print(f"value user chose: {val_chosen}")
-        print(type(val_chosen))
-        dff = df[df["fund_extended_name"].isin(val_chosen)]
-        fig = px.pie(dff, values="ytd_return", names="fund_extended_name", title="Year-to-Date Returns")
-        fig.update_traces(textinfo="value+percent").update_layout(title_x=0.5)
-        return fig
-    elif len(val_chosen) == 0:
-        raise dash.exceptions.PreventUpdate
+])
+
+
 
 
 
