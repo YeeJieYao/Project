96e0919affc0f8c45f7881559dfcbd5faa0b7783
YeeJieYao <1221402113@student.mmu.edu.my>
2023-09-08 17:48:10 +0800

    checklist

--- testp.py
+++ testp.py
@@ -21,5 +21,10 @@ app.layout = html.Div(
     dcc.Graph(figure = fig)]
 )
 
+app.layout = html.Div([
+dcc.Checklist(['Johor', 'Kedah', 'Kelantan','Melaka', 'Negeri Sembilan', 'Pahang', 'Pulau Pinang', 'Perak', 'Perlis', 'Selangor', 'Terengganu', 'Sabah', 'Sarawak', 'W.P. Kuala Lumpur', 'W.P. Labuan', 'W.P. Putrajaya'],
+              ['Johor', 'Kedah', 'Kelantan','Melaka', 'Negeri Sembilan', 'Pahang', 'Pulau Pinang', 'Perak', 'Perlis', 'Selangor', 'Terengganu', 'Sabah', 'Sarawak', 'W.P. Kuala Lumpur', 'W.P. Labuan', 'W.P. Putrajaya'])
+])
+
 if __name__ == '__main__':
     app.run_server(debug=True)
