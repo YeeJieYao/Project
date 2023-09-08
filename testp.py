01eebb22973b5032f25c5219390eeac70d50c84e
YeeJieYao <1221402113@student.mmu.edu.my>
2023-09-08 18:24:03 +0800

    checklist

--- testp.py
+++ testp.py
@@ -13,17 +13,26 @@ df2= df.iloc[1:]
 
 fig = px.pie(df2,values='pop', names='state',title='Malaysia Population') .update_layout(xaxis_title="State", yaxis_title="Index")
 
-dcc.Graph(figure = fig)
 
 app.layout = html.Div(
     [html.Img(src=image_path),
     html.H1("Data Visualization"),
     html.H2("Dashboard showing graphs"),
     dcc.Checklist(['Johor', 'Kedah', 'Kelantan','Melaka', 'Negeri Sembilan', 'Pahang', 'Pulau Pinang', 'Perak', 'Perlis', 'Selangor', 'Terengganu', 'Sabah', 'Sarawak', 'W.P. Kuala Lumpur', 'W.P. Labuan', 'W.P. Putrajaya'],
-              'Johor', id='my-checklist')]
+              'Johor', id='my-checklist'),
+    dcc.Graph(id='graph-output', figure = fig)]
 )
 
 
+@app.callback(
+    Output(component_id='graph-output', component_property='figure'),
+    Input(component_id='my-checklist', component_property='values')
+)
+
+def update_my_graph(dropdown_chosen, color_chosen):
+    fig = px.pie(df2,values='pop', names='state',title='Malaysia Population') .update_layout(xaxis_title="State", yaxis_title="Index")
+    fig.update_traces(line_color=color_chosen['hex'])
+    return fig
 
 
 if __name__ == '__main__':
