8a71d72bb37638648862ec512e9f312075973b55
YeeJieYao <1221402113@student.mmu.edu.my>
2023-09-09 08:11:38 +0800

    asd

--- testp.py
+++ testp.py
@@ -1,61 +1,83 @@
 import pandas as pd
 import plotly.express as px
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
-df = pd.read_csv("https://raw.githubusercontent.com/YeeJieYao/Project/main/population.csv")
-
-colors = ["black", "blue", "red", "yellow", "pink", "orange"]
-
 app = dash.Dash(__name__)
 server = app.server
 
+# Declare DataFrame and File Paths
+df = pd.read_csv("https://raw.githubusercontent.com/YeeJieYao/Project/main/population.csv")
 image_path = 'https://www.mmu.edu.my/wp-content/themes/mmu2018/assets/images/logo-mmu.png'
 
+# Declare Data Fields
+stateSelected = ["Johor", "Perak", "Melaka"]
+colors = ["Black", "Blue", "Red", "Yellow", "Pink", "Orange"]
+
+# HTML Layout
 app.layout = html.Div(
     children=[
     html.Img(src=image_path),	                
     html.H1("Data Visualization"),
     html.H2("Dashboard showing graphs"),
-
-        dcc.Dropdown(id='my-dropdown', multi=True,
-                     options=[{'label': x, 'value': x} for x in sorted(df.state.unique())],
-                     value=["Johor"]),
-        html.Button(id='my-button', n_clicks=0, children="Show all"),
-        dcc.Graph(id='graph-output', figure={}),
-
-        html.Div(id="sentence-output", children=["This is the color I love"], style={}),
-        dcc.RadioItems(id='my-radioitem', value="black", options=[{'label': c, 'value': c} for c in colors]),
+    html.H1('Dash Tabs component demo'),
+    dcc.Tabs(id="tabs-example-graph", value='tab-1-example-graph', children=[
+    dcc.Tab(label='Tab One', value='tab-1-example-graph'),
+    dcc.Tab(label='Tab Two', value='tab-2-example-graph'),
+    ]),
+    html.Div(id='tabs-content-example-graph')
     ]
 )
 
-# Single Input, single Output, State, prevent initial trigger of callback, PreventUpdate
+# Graph and Color Function
 @app.callback(
-    Output(component_id='graph-output', component_property='figure'),
-    [Input(component_id='my-dropdown', component_property='value')],
-    #Input(component_id='my-button', component_property='n_clicks')],
-    # [State(component_id='my-dropdown', component_property='value')],
+    [Output('graph-output', 'figure'), Output('sentence-output', 'style')],
+    [Input(component_id='my-radioitem', component_property='value'),
+    Input(component_id='my-dropdown', component_property='value'),],
     prevent_initial_call=False
 )
-def update_my_graph(val_chosen):
-    if len(val_chosen) > 0:
-        # print(n)
-        print(f"value user chose: {val_chosen}")
-        print(type(val_chosen))
+
+def update_graph(color_chosen, val_chosen):
+    if len(val_chosen) == 0:
+        return dash.no_update, {"color": color_chosen}
+    else:
         dff = df[df["state"].isin(val_chosen)]
         fig = px.pie(dff, values="pop", names="state", title="Malaysia Population")
         fig.update_traces(textinfo="value+percent").update_layout(title_x=0.5)
-        return fig
-    
-    
-    
-    elif len(val_chosen) == 0:
-        raise dash.exceptions.PreventUpdate
-    
-    
+        return fig, {"color": color_chosen}
 
+# Download CSV Function
+@app.callback(
+    Output("download-dataframe-csv", "data"),
+    Input("btn_csv", "n_clicks"),
+    prevent_initial_call=True,
+)
+def func(n_clicks):
+    return dcc.send_data_frame(df.to_csv, "malaysiapopulation.csv")
+
+# Tab Function
+@app.callback(Output('tabs-content-example-graph', 'children'), Input('tabs-example-graph', 'value'))
+
+def render_content(tab):
+    if tab == 'tab-1-example-graph':
+        return html.Div([
+            html.Br(),
+            html.H1(id="sentence-output", children=["Malaysia Population by State"], style={}),
+            dcc.Dropdown(id='my-dropdown', multi=True, options=[{'label': x, 'value': x} for x in sorted(df.state.unique())], value=stateSelected),
+            dcc.Graph(id='graph-output', figure={}),
+            html.H2(children=["Choose a color for the title"], style={}),
+            dcc.RadioItems(id='my-radioitem', value="black", options=[{'label': c, 'value': c} for c in colors]),
+        ])
+    elif tab == 'tab-2-example-graph':
+        return html.Div([
+            html.Br(),
+            html.H1(id="sentence-output2", children=["Download the CSV File"], style={}),
+            html.Button("Download CSV", id="btn_csv"),
+            dcc.Download(id="download-dataframe-csv"),
+        ])   
+
+# Main    
 if __name__ == '__main__':
     app.run_server(debug=True)
